9aff7e05d4824abc52fcdd9ce35ba6730e331466
Viktor Afanasjev <viktor85a@gmail.com>
2017-05-30 16:23:17 +0300

    add database

--- database/yan.sql
+++ database/yan.sql
@@ -11,14 +11,160 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Экспортируемые данные не выделены.
--- Экспортируемые данные не выделены.
--- Экспортируемые данные не выделены.
--- Экспортируемые данные не выделены.
--- Экспортируемые данные не выделены.
--- Экспортируемые данные не выделены.
--- Экспортируемые данные не выделены.
--- Экспортируемые данные не выделены.
+
+-- Дамп структуры базы данных yan
+CREATE DATABASE IF NOT EXISTS `yan` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `yan`;
+
+-- Дамп структуры для таблица yan.items
+CREATE TABLE IF NOT EXISTS `items` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы yan.items: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` (`id`, `title`, `description`, `created_at`, `updated_at`) VALUES
+	(1, 'testItem', 'Description', '2017-05-30 11:50:59', '2017-05-30 11:50:59');
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+
+-- Дамп структуры для таблица yan.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы yan.migrations: ~4 rows (приблизительно)
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+	(7, '2014_10_12_000000_create_users_table', 1),
+	(8, '2014_10_12_100000_create_password_resets_table', 1),
+	(9, '2017_05_30_103825_create_items_table', 1),
+	(10, '2017_05_30_103914_entrust_setup_tables', 1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+
+-- Дамп структуры для таблица yan.password_resets
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы yan.password_resets: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+
+-- Дамп структуры для таблица yan.permissions
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `display_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `permissions_name_unique` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы yan.permissions: ~8 rows (приблизительно)
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` (`id`, `name`, `display_name`, `description`, `created_at`, `updated_at`) VALUES
+	(1, 'role-list', 'Display Role Listing', 'See only Listing Of Role', '2017-05-30 11:41:43', '2017-05-30 11:41:43'),
+	(2, 'role-create', 'Create Role', 'Create New Role', '2017-05-30 11:41:43', '2017-05-30 11:41:43'),
+	(3, 'role-edit', 'Edit Role', 'Edit Role', '2017-05-30 11:41:43', '2017-05-30 11:41:43'),
+	(4, 'role-delete', 'Delete Role', 'Delete Role', '2017-05-30 11:41:43', '2017-05-30 11:41:43'),
+	(5, 'item-list', 'Display Item Listing', 'See only Listing Of Item', '2017-05-30 11:41:43', '2017-05-30 11:41:43'),
+	(6, 'item-create', 'Create Item', 'Create New Item', '2017-05-30 11:41:43', '2017-05-30 11:41:43'),
+	(7, 'item-edit', 'Edit Item', 'Edit Item', '2017-05-30 11:41:43', '2017-05-30 11:41:43'),
+	(8, 'item-delete', 'Delete Item', 'Delete Item', '2017-05-30 11:41:43', '2017-05-30 11:41:43');
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+
+-- Дамп структуры для таблица yan.permission_role
+CREATE TABLE IF NOT EXISTS `permission_role` (
+  `permission_id` int(10) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`permission_id`,`role_id`),
+  KEY `permission_role_role_id_foreign` (`role_id`),
+  CONSTRAINT `permission_role_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `permission_role_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы yan.permission_role: ~9 rows (приблизительно)
+/*!40000 ALTER TABLE `permission_role` DISABLE KEYS */;
+INSERT INTO `permission_role` (`permission_id`, `role_id`) VALUES
+	(1, 1),
+	(1, 2),
+	(2, 1),
+	(2, 2),
+	(3, 1),
+	(4, 1),
+	(5, 1),
+	(6, 1),
+	(7, 1),
+	(8, 1);
+/*!40000 ALTER TABLE `permission_role` ENABLE KEYS */;
+
+-- Дамп структуры для таблица yan.roles
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `display_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `roles_name_unique` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы yan.roles: ~2 rows (приблизительно)
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` (`id`, `name`, `display_name`, `description`, `created_at`, `updated_at`) VALUES
+	(1, 'admin', 'admin', 'administrator', '2017-05-30 14:43:29', '2017-05-30 14:43:31'),
+	(2, 'webmaster', 'webmaster', 'webmaster', '2017-05-30 11:51:31', '2017-05-30 11:51:31');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+
+-- Дамп структуры для таблица yan.role_user
+CREATE TABLE IF NOT EXISTS `role_user` (
+  `user_id` int(10) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `role_user_role_id_foreign` (`role_id`),
+  CONSTRAINT `role_user_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `role_user_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы yan.role_user: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `role_user` DISABLE KEYS */;
+INSERT INTO `role_user` (`user_id`, `role_id`) VALUES
+	(1, 1);
+/*!40000 ALTER TABLE `role_user` ENABLE KEYS */;
+
+-- Дамп структуры для таблица yan.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы yan.users: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+	(1, 'webmaster', 'webmaster@yan.dev', '$2y$10$7j2C9Gnh2ukxVuzQdXPk..g0VVhY5pfFcMFejpdser1IsA3JiBWGm', NULL, '2017-05-30 11:42:48', '2017-05-30 11:42:48');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
